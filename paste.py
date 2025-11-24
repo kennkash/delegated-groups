909991a57f70f66ba9a47302e1f39791433d13ac
kennkash <kkashmiry23@gmail.com>
2025-11-24 10:11:11 -0600

    Update paste.py

--- paste.py
+++ paste.py
@@ -18,6 +18,15 @@ TO "atlassian_admin.dev";
 GRANT SELECT
 ON "atlassian-admin.dev".vw_delegated_group_owners
 TO "atlassian_admin.dev";
+
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA "atlassian-admin.dev"
+GRANT SELECT, INSERT, UPDATE, DELETE
+ON TABLES TO "atlassian_admin.dev";
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA "atlassian-admin.dev"
+GRANT USAGE, SELECT
+ON SEQUENCES TO "atlassian_admin.dev";
 """
 
 with models.engine.begin() as conn:
